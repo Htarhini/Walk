736172315733d72dfb1f86dd187df1e02261a2dd
freetime <nouratwy@hotmail.com>
2018-10-04 23:35:27 +0300

    hello

--- MainPage.php
+++ MainPage.php
@@ -1,16 +1,1 @@
-<<<<<<< HEAD
-<html>
-<head>
-<title>Main Page</title>
-</head>
-
-<body>
-
-<P>hello world</p>
-<h1>Mahdi is a bitch</h1>
-</body>
-</html>
-
-=======
 <P>hello my world</p>
->>>>>>> cdf02187d5cd57347cfb5b840b720d33f1551122
